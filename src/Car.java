23778b185042019d5890d625e35f9dcecdc79e84
DaviBarretoo <davibarretojkda@gmail.com>
2025-01-04 20:34:00 -0300

    First method

---
+++ src/Car.java
@@ -0,0 +1,9 @@
+public class Car {
+    boolean ligado = false;
+    int gasolina = 1;
+
+public void isOn(){
+    ligado= true;
+    System.out.println("The car is On now");
+}
+}
---
+++ src/Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+    public static void main(String[] args) {
+        Car Astra = new Car();
+        Astra.isOn();;
+    }
+
+}
